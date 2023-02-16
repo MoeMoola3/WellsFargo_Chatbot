7b6ee4f1ecc4042fe8e033600ec15cd38601d3a7
MoeMoola3 <73942516+MoeMoola3@users.noreply.github.com>
2023-02-16 22:50:45 +0200

    testDeploy

--- chatbot.py
+++ chatbot.py
@@ -64,13 +64,14 @@
 
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    return render_template('test.html')
 
-if __name__ == "__main__":
-    app.run()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
